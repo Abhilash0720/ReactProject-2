330674ae37799ef2fbc233916301cc3daa4f5995
Abhilash0720 <abhilash20702@gmail.com>
2023-10-13 15:50:01 +0530

    style updated

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 function MainPage() {
   return<>
+  <center>
    <div class="heading" >
     <h2 class="add">Add a Note</h2>
     <h3 class="add">Title</h3>
    <h4>Take a note...</h4>
    </div>
-   
+   </center>
   </>
 }
 
